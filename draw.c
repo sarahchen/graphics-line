e243621d8f0bb3d8a98dbc0a42696d33671720cd
Sarah Chen <sarah.jia.chen@gmail.com>
2016-02-25 01:07:09 -0800

    push

--- draw.c
+++ draw.c
@@ -7,5 +7,60 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
-}
+  int A = y1 - y0;
+  int B = x0 - x1;
+  float m = -((float)A/ (float)B);
+  int d = (2*A) + B;
 
+  int x = x0;
+  int y = y0;
+  // 0 < m < 1 (Octant 1)
+  if(0 <= m && m <= 1) {
+    while(x < x1) {
+      plot(s, c, x, y);
+      if(d > 0) {
+        y = y + 1;
+        d = d + (2*B);
+      }
+      x = x+1;
+      d = d + (2*A);
+    }
+  }
+  // m > 1 (Octant 2)
+  else if(m > 1) {
+    while(y < y0) {
+      plot(s, c, x, y);
+      if(d < 0) {
+        x = x + 1;
+        d = d + (2*A);
+      }
+      y = y+1;
+      d = d + (2*B);
+    }
+  }
+  // -1 < m < 0 (Octant 8)
+  else if(-1<=m && m<=0) {
+    while(x < x1) {
+      plot(s, c, x, y);
+      if(d < 0) {
+        y = y-1;
+        d = d - (2*B);
+      }
+      x = x+1;
+      d = d + (2*A);
+    }
+  }
+  // m < -1 (Octant 7)
+  else if(m<(-1)) {
+    while(y < y1) {
+      plot(s, c, x, y);
+      if(d > 0) {
+        x = x+1;
+        d = d + (2*A);
+      }
+      y = y-1;
+      d = d-(2*B);
+    }
+  }
+  
+}
--- main.c
+++ main.c
@@ -10,24 +10,24 @@ int main() {
 
   screen s;
   color c;
- 
-  
+
+
   c.red = 0;
   c.green = MAX_COLOR;
   c.blue = 0;
-  
+
   clear_screen(s);
 
 
   //octant 1
   draw_line( 0, 0, XRES-1, YRES - 75, s, c);
   //octant 2
-  draw_line( 0, 0, XRES - 75, YRES-1, s, c); 
+  draw_line( 0, 0, XRES - 75, YRES-1, s, c);
   //octant 8
-  draw_line( 0, YRES-1, XRES-1, 75, s, c);  
+  draw_line( 0, YRES-1, XRES-1, 75, s, c);
   //octant 7
   draw_line( 0, YRES-1, XRES - 75, 0, s, c);
-
+/*
   c.green = 0;
   c.blue = MAX_COLOR;
   //octant 5
@@ -38,7 +38,7 @@ int main() {
   draw_line( XRES - 1, 0, 0, YRES - 75, s, c);
   //octant 3
   draw_line( XRES - 1, 0, 75, YRES - 1, s, c);
-  
+
   c.blue = 0;
   c.red = MAX_COLOR;
   //y = x, y = -x
@@ -48,7 +48,9 @@ int main() {
   //horizontal, vertical line
   draw_line( 0, YRES / 2, XRES - 1, YRES / 2, s, c);
   draw_line( XRES / 2, 0, XRES / 2, YRES - 1, s, c);
-  
+  */
+
+
   display(s);
   save_extension(s, "lines.png");
-}  
+}
